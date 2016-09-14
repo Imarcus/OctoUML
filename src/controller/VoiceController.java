e421e2a247b82def2742af60675ef1b858900136
Marcus <marcus.i@ive.se>
2016-09-14 10:35:23 +0200

    Updates name of grammar file

--- src/controller/VoiceController.java
+++ src/controller/VoiceController.java
@@ -59,8 +59,7 @@ public class VoiceController {
                     .setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
             configuration
                     .setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
-            //set the path to the file "hello.gram"
-            configuration.setGrammarPath("file:C:/Users/chalmers/gitrepos/PenguinUML/src/controller/"); //Laptop
+            //set the path to the file "octo.gram"
             configuration.setGrammarPath("file:C:/Users/Marcus/Documents/mthubrepo/PenguinUML"); //Home PC
             configuration.setGrammarName("hello");
             configuration.setUseGrammar(true);
